c08594ae9126b9dffcc97b3ad51790afa11a0d59
superspeeder <andy@delusion.org>
2025-04-18 11:45:08 -0700

    starting work on pipelines

--- src/neuron/pipeline.cpp
+++ src/neuron/pipeline.cpp
@@ -1,5 +1,57 @@
 #include "pipeline.hpp"
 
+#include <fstream>
+#include <stdexcept>
+
 namespace neuron {
-    
-}
+    ShaderModule::ShaderModule(const std::shared_ptr<RenderSystem> &renderSystem, const std::filesystem::path &path) : m_ShaderModule(nullptr) {
+        std::ifstream file(path, std::ios::binary | std::ios::in | std::ios::ate);
+        if (!file.is_open()) {
+            throw std::invalid_argument("Shader file couldn't be opened");
+        }
+
+        const auto end = file.tellg();
+        file.seekg(0, std::ios::beg);
+        auto code = std::vector<uint32_t>(end / sizeof(uint32_t), 0);
+        file.read(reinterpret_cast<char *>(code.data()), code.size() * sizeof(uint32_t));
+        file.close();
+
+        vk::ShaderModuleCreateInfo createInfo{};
+        createInfo.setCode(code);
+        m_ShaderModule = vk::raii::ShaderModule(renderSystem->device(), createInfo);
+    }
+
+    ShaderModule::ShaderModule(const std::shared_ptr<RenderSystem> &renderSystem, const std::vector<uint32_t> &code) : m_ShaderModule(renderSystem->device(), {{}, code}) {}
+
+    GraphicsPipeline::GraphicsPipeline(const std::shared_ptr<RenderSystem> &renderSystem, const Description &description) {
+        std::vector<vk::PipelineShaderStageCreateInfo> shaderStages;
+        for (const auto &[module, stage, entry] : description.shaderStages) {
+            shaderStages.emplace_back({}, stage, module->handle(), entry.c_str());
+        }
+
+        vk::PipelineVertexInputStateCreateInfo vertexInputState{};
+        vertexInputState.setVertexBindingDescriptions(description.vertexInputLayout.bindings).setVertexAttributeDescriptions(description.vertexInputLayout.attributes);
+
+        vk::PipelineInputAssemblyStateCreateInfo inputAssembly{};
+        inputAssembly.setTopology(description.inputAssembler.topology);
+        inputAssembly.setPrimitiveRestartEnable(description.inputAssembler.allowPrimitiveRestart);
+
+        vk::PipelineViewportStateCreateInfo viewportState{};
+        std::vector<vk::Viewport> viewports;
+        std::vector<vk::Rect2D> scissors;
+        for (const auto& [viewport, scissor] : description.viewports) {
+            viewports.emplace_back(viewport);
+            scissors.emplace_back(scissor);
+        }
+        viewportState.setViewports(viewports);
+        viewportState.setScissors(scissors);
+
+        vk::PipelineRasterizationStateCreateInfo rasterizer{};
+        rasterizer.setRasterizerDiscardEnable(description.rasterizerState.discardOutput);
+        rasterizer.setPolygonMode(description.rasterizerState.polygonMode);
+        rasterizer.setCullMode(description.rasterizerState.cullMode);
+        rasterizer.setFrontFace(description.rasterizerState.frontFace);
+        rasterizer.setDepthClampEnable(description.rasterizerState.clampDepth);
+        
+    }
+} // namespace neuron
--- src/neuron/pipeline.hpp
+++ src/neuron/pipeline.hpp
@@ -1,9 +1,93 @@
 #pragma once
+#include "render_system.hpp"
+
+
+#include <filesystem>
+#include <memory>
 
 namespace neuron {
     class ShaderModule {
-    public:
-    private:
-    
-    }
-}
+      public:
+        ShaderModule(const std::shared_ptr<RenderSystem> &renderSystem, const std::filesystem::path &path);
+        ShaderModule(const std::shared_ptr<RenderSystem> &renderSystem, const std::vector<uint32_t> &code);
+
+        inline const vk::raii::ShaderModule &handle() const { return m_ShaderModule; };
+
+      private:
+        vk::raii::ShaderModule m_ShaderModule;
+    };
+
+    struct VertexInputLayout {
+        std::vector<vk::VertexInputBindingDescription>   bindings;
+        std::vector<vk::VertexInputAttributeDescription> attributes;
+    };
+
+    struct InputAssemblerState {
+        vk::PrimitiveTopology topology              = vk::PrimitiveTopology::eTriangleList;
+        bool                  allowPrimitiveRestart = false;
+    };
+
+    struct RasterizerDepthBias {
+        float constantFactor = 0.0f;
+        float slopeFactor    = 0.0f;
+        float clamp          = 0.0f;
+    };
+
+    struct RasterizerState {
+        vk::PolygonMode                    polygonMode   = vk::PolygonMode::eFill;
+        vk::CullModeFlags                  cullMode      = vk::CullModeFlagBits::eBack;
+        vk::FrontFace                      frontFace     = vk::FrontFace::eClockwise;
+        float                              lineWidth     = 1.0f;
+        bool                               clampDepth    = false;
+        bool                               discardOutput = false;
+        std::optional<RasterizerDepthBias> depthBias     = std::nullopt;
+    };
+
+    struct TessellatorState {
+        uint32_t patchControlPoints = 1;
+    };
+
+    struct RenderingCompatibility {
+        std::vector<vk::Format> colorFormats;
+        vk::Format              depthFormat   = vk::Format::eUndefined;
+        vk::Format              stencilFormat = vk::Format::eUndefined;
+    };
+
+    struct MultisamplingState {
+        vk::SampleCountFlagBits     rasterizationSamples  = vk::SampleCountFlagBits::e1;
+        float                       minSampleShading      = 0.0f;
+        bool                        enableSampleShading   = false;
+        std::vector<vk::SampleMask> sampleMask            = {};
+        bool                        enableAlphaToOne      = false;
+        bool                        enableAlphaToCoverage = false;
+    };
+
+    struct ColorBlendingState {
+        std::vector<vk::PipelineColorBlendAttachmentState> attachments;
+
+        std::array<float, 4>       blendConstants = {0.0f, 0.0f, 0.0f, 0.0f};
+        std::optional<vk::LogicOp> logicOp        = std::nullopt;
+    };
+
+    class GraphicsPipeline {
+      public:
+        struct Description {
+            std::vector<std::tuple<const ShaderModule *, vk::ShaderStageFlagBits, std::string>> shaderStages;
+            RenderingCompatibility                                                              renderingCompatibility;
+
+            std::vector<std::pair<vk::Viewport, vk::Rect2D>> viewports;
+            std::vector<vk::DynamicState>                    dynamicStates;
+
+            VertexInputLayout   vertexInputLayout{};
+            InputAssemblerState inputAssembler{};
+            RasterizerState     rasterizerState{};
+            TessellatorState    tessellatorState{};
+            MultisamplingState  multisamplingState{};
+            ColorBlendingState  colorBlendingState{};
+        };
+
+        GraphicsPipeline(const std::shared_ptr<RenderSystem> &renderSystem, const Description &description);
+
+      private:
+    };
+} // namespace neuron
